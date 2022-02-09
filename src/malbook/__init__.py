58343a0efc4a6c4db0c9f8fe4cf06c0d0424b86f
Rida <38199959+rdzhaafar@users.noreply.github.com>
2022-02-09 11:32:54 +0300

    Finished setting up bumpversion

--- src/malbook/__init__.py
+++ src/malbook/__init__.py
@@ -1,5 +1,5 @@
 from .framework import Notebook, Pipeline, Error, Callback, make_pipeline
 
 __all__ = ['Notebook', 'Pipeline', 'Error', 'Callback', 'make_pipeline']
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
