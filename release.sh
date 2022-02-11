beef054a9ce577077cb10efc78cf5e196ca92807
Rida <38199959+rdzhaafar@users.noreply.github.com>
2022-02-11 11:09:40 +0300

    `release.sh` now prompts for confirmation

--- release.sh
+++ release.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
-# Sets up, builds and publishes a new version of the package
+# Releases a new version of malbook into the wild
 
 test -z $VIRTUAL_ENV && echo "'release.sh' must be run from a virtual environment" && exit 1
 
-bumpversion patch
-git push origin main
-python3 -m build
-python3 -m twine upload dist/*
+echo "WARNING: This script will release the repository to PyPi as-is, meaning"
+echo "that any bugs or unwanted details will end up in public."
+read -p "Are you sure you want to proceed? (Y/n) " -r
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    echo "Bumping the version number"
+    bumpversion patch
+    echo "Pushing changes to GitHub..."
+    git push origin main
+
+    echo "Building..."
+    python3 -m build
+
+    echo "Uploading to PyPi..."
+    python3 -m twine upload dist/*
+fi
